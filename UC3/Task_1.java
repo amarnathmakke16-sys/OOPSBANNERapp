ceb9a211d2b0f1171c9d1c5671253c7045a6ace6
amarnathmakke16-sys <amarnathmakke16@gmail.com>
2026-03-11 13:52:23 +0530

    uc2 and uc3

---
+++ UC3/Task_1.java
@@ -0,0 +1,15 @@
+public class Task_1 {
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ", "   ***   ","   ***   ","******   ","   ***** "));
+        System.out.println(String.join(" ", " **   ** "," **   ** ","**    ** "," **      "));
+        System.out.println(String.join(" ", "**     **","**     **","**     **","**       "));
+        System.out.println(String.join(" ", "**     **","**     **","**    ** "," **      "));
+        System.out.println(String.join(" ", "**     **","**     **","******   ","   ***   "));
+        System.out.println(String.join(" ", "**     **","**     **","**       ","      ** "));
+        System.out.println(String.join(" ", "**     **","**     **","**       ","       **"));
+        System.out.println(String.join(" ", " **   ** "," **   ** ","**       ","      ** "));
+        System.out.println(String.join(" ", "   ***   ","   ***   ","**       "," *****   "));
+
+}   
+}
